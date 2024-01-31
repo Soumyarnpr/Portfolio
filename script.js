4584025e7ba0cf66a516ed6d3a38ad85921b19cc
Soumyarnpr <soumyarnpr@gmail.com>
2024-01-31 20:24:38 +0530

    Changed style for more responsivness & added new WEB-DEV lang.

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ window.onscroll = function () {
 
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const nav = document.getElementById('nav');
 
 if (bar) {
     bar.addEventListener('click', function () {
-            nav.classList.add('active');
-        })
+        nav.classList.add('active');
+        bar.style.display = "none";
+    })
 }
 
 if (close) {
     close.addEventListener('click', function () {
-            nav.classList.remove('active');
-        })
+        nav.classList.remove('active');
+        bar.style.display = "block";
+    })
 }
-
-
